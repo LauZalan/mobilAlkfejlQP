ef1949c7f1a88a3364d8d34eafe2977e0ef5d77d
LauZalan <laurinyeczzalan@gmail.com>
2025-05-17 20:18:06 +0200

    Small progress toward ssecond milestone

--- app/src/main/java/com/example/quickprint/HomeActivity.java
+++ app/src/main/java/com/example/quickprint/HomeActivity.java
@@ -9,19 +9,42 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
 
     private FirebaseUser user;
     private FirebaseAuth auth;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +61,88 @@ public class HomeActivity extends AppCompatActivity {
 
         int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
 
-        TextView textViewBanner = findViewById(R.id.textViewQuickPrintLogin);
         TextView textViewBig = findViewById(R.id.yay);
         TextView textViewSmall = findViewById(R.id.oops);
         Button button = findViewById(R.id.logout);
 
         if (secret_key == 99) {
-            textViewBanner.setText("Yay!");
             textViewBig.setText("Successful Login!");
-            textViewSmall.setText("This place is under contruction :/ You can sign out with this cool button though:");
+            textViewSmall.setText("This place is under construction :/ You can sign out with this cool button though:");
             button.setVisibility(View.VISIBLE);
         }
 
         if (user != null) {
-
             Log.i(TAG, "Yay!");
-            Toast.makeText(HomeActivity.this, "Logged in as: " + user.getEmail().toString(),Toast.LENGTH_LONG).show();
+
+            if (user.getEmail() == null) {
+                Toast.makeText(HomeActivity.this, "Logged in as: Anonymous",Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(HomeActivity.this, "Logged in as: " + user.getEmail(),Toast.LENGTH_LONG).show();
+            }
         } else {
             Log.i(TAG, "Ooops!");
             finish();
         }
+
+        drawerLayout = findViewById(R.id.main);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+
+        // Add the toggle as a listener to the DrawerLayout
+        drawerLayout.addDrawerListener(toggle);
+
+        // Synchronize the toggle's state with the linked DrawerLayout
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // Called when an item in the NavigationView is selected.
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle the selected item based on its ID
+                if (item.getItemId() == R.id.nav_account) {
+                    // Show a Toast message for the Account item
+                    Toast.makeText(HomeActivity.this,
+                            "Account Details", Toast.LENGTH_SHORT).show();
+                }
+
+                if (item.getItemId() == R.id.nav_settings) {
+                    // Show a Toast message for the Settings item
+                    Toast.makeText(HomeActivity.this,
+                            "Settings Opened", Toast.LENGTH_SHORT).show();
+                }
+
+                if (item.getItemId() == R.id.nav_logout) {
+                    // Show a Toast message for the Logout item
+                    Toast.makeText(HomeActivity.this,
+                            "You are Logged Out", Toast.LENGTH_SHORT).show();
+                }
+
+                // Close the drawer after selection
+                drawerLayout.closeDrawers();
+                // Indicate that the item selection has been handled
+                return true;
+            }
+        });
+
+        // Add a callback to handle the back button press
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            // Called when the back button is pressed.
+            @Override
+            public void handleOnBackPressed() {
+                // Check if the drawer is open
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    // Close the drawer if it's open
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    // Finish the activity if the drawer is closed
+                    finish();
+                }
+            }
+        });
     }
 
     public void home(View view) {
@@ -67,6 +152,9 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void logout(View view) {
+        if (user.getEmail() == null) {
+            user.delete();
+        }
         Toast.makeText(HomeActivity.this, "Logged out!",Toast.LENGTH_LONG).show();
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this, MainActivity.class);
--- app/src/main/java/com/example/quickprint/MainActivity.java
+++ app/src/main/java/com/example/quickprint/MainActivity.java
@@ -11,20 +11,29 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    private static final int RC_SIGN_IN = 420;
     private static final int SECRET_KEY = 99;
 
     EditText userNameET;
@@ -33,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences preferences;
 
     private FirebaseAuth auth;
+    private GoogleSignInClient gAuth;
 
 
     @Override
@@ -53,6 +63,44 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        gAuth = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    private void authWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i(TAG, "Yay!");
+                    Success();
+                } else {
+                    Log.i(TAG, "Opps!");
+                    Toast.makeText(MainActivity.this, "Oops! Something went wrong! " + task.getException().getMessage() ,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                authWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w(TAG, "Google sign in failed...", e);
+            }
+        }
     }
 
     public void login(View view) {
@@ -83,11 +131,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loginAsGuest(View view) {
-        Toast.makeText(MainActivity.this, "Oops! This feature is not available yet. Sorry :/",Toast.LENGTH_LONG).show();
+        auth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i(TAG, "Yay!");
+                    Success();
+                } else {
+                    Log.i(TAG, "Opps!");
+                    Toast.makeText(MainActivity.this, "Oops! Something went wrong! " + task.getException().getMessage() ,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        // Toast.makeText(MainActivity.this, "Oops! This feature is not available yet. Sorry :/",Toast.LENGTH_LONG).show();
     }
 
     public void loginGoogle(View view) {
-        Toast.makeText(MainActivity.this, "Oops! This feature is not available yet. Sorry :/",Toast.LENGTH_LONG).show();
+        Intent signInIntent = gAuth.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        //Toast.makeText(MainActivity.this, "Oops! This feature is not available yet. Sorry :/",Toast.LENGTH_LONG).show();
     }
 
     public void register(View view) {
